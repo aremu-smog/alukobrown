4f832737d16338806d1f549c52fa81fa1b9fa0ff
Aremu Smog <aremuoluwagbamila@gmail.com>
2020-11-15 11:49:29 +0100

    Fix: Cursor and mobile responsiveness

--- src/components/cursor.js
+++ src/components/cursor.js
@@ -3,24 +3,43 @@ import gsap from "gsap"
 import CSSRulePlugin from 'gsap/CSSRulePlugin'
 const Cursor = () => {
     const cursor = useRef()
-    const follower = CSSRulePlugin.getRule(".cursor::after")
+    const follower = CSSRulePlugin.getRule(".cursor:after")
+
+    
     useEffect(()=> {
+
+        const return_int_of = (string) => {
+            return typeof(string) === "string" ? parseInt(string, 10) : string
+
+        }
+        
         document.addEventListener("mousemove", (e)=> {
-            const prev_top = cursor.current.style.top
-            const prev_left = cursor.current.style.left
+            const prev_top = return_int_of(cursor.current.style.top)
+            const prev_left = return_int_of(cursor.current.style.left)
 
-            const new_top = `${e.clientY}px`
-            const new_left = `${e.clientX}px`
-            if(prev_top == new_top && prev_left == new_left){
-                gsap.to(follower, {width: "30px", height: "30px", borderRadius: "50%", duration: 0.3})
-            }else{
-                gsap.to(follower, {width: "50px", height: "30px", borderRadius: "5px", duration: 0.3})
+            const new_top = e.clientY
+            const new_left = e.clientX
 
-            }
+            //Change in Y position
+            const d_top = new_top - prev_top
+
+            // //Change in X position
+            const d_left = new_left - prev_left
+
+            // const angle = Math.round(Math.atan(d_left/d_top))
+
+            // console.log(prev_left, new_left, d_left)
             cursor.current.style.top = `${e.clientY}px`
             cursor.current.style.left = `${e.clientX}px`
+            // if(prev_top === new_top && prev_left === new_left){
+            //     gsap.to(follower, {width: "30px", height: "30px", borderRadius: "50%", duration: 0.1})
+            // }else{
+            //     gsap.to(follower, {width: "50px", height: "30px", rotate: 30, borderRadius: "5px", duration: 0.1})
+
+            // }
         })
     },)
+
     return (
         <div className="cursor" ref={cursor}></div>
     )
