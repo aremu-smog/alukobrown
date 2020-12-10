fd545a98bc65de93d91764ece31a132f0e72d9c0
Aremu Smog <aremuoluwagbamila@gmail.com>
2020-12-10 13:11:52 +0100

    Feat: Cursor v2.0

--- src/components/cursor.js
+++ src/components/cursor.js
@@ -2,14 +2,23 @@ import React, { useEffect, useRef } from "react"
 
 const Cursor = () => {
   const cursor = useRef()
+  const cursorInner = useRef()
   useEffect(() => {
     document.addEventListener("mousemove", e => {
-      cursor.current.style.top = `${e.clientY}px`
-      cursor.current.style.left = `${e.clientX}px`
+      cursor.current.style.top = (e.clientY - 16)+'px'
+      cursorInner.current.style.top = (e.clientY - 5)+'px'
+      cursor.current.style.left = (e.clientX - 16)+'px'
+      cursorInner.current.style.left = (e.clientX - 5)+'px'
     })
   })
 
-  return <div className="cursor" ref={cursor}></div>
+  return (
+    <>
+      <div className="cursor" ref={cursor}></div>
+    <div className="cursorInner" ref={cursorInner}></div>
+    </>
+  
+  )
 }
 
 export default Cursor
